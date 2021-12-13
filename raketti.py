63864b188afaf79693b45027cda74c0feb15fe46
Tuomas Suutari <tuomas@nepnep.net>
2021-12-13 20:58:05 +0200

    Tyylikorjauksia

--- raketti.py
+++ raketti.py
@@ -36,16 +36,16 @@ class Peli:
         self.naytto = pygame.display.set_mode(
             self.nayton_koko, pygame.HWSURFACE | pygame.DOUBLEBUF)
         self.raketti_iso = pygame.image.load("rocket_883.png")
-        self.raketti_pieni = pygame.transform.rotozoom(self.raketti_iso, 0, 0.25)
+        self.raketti = pygame.transform.rotozoom(self.raketti_iso, 0, 0.25)
         self.juttu_iso = pygame.image.load("astronautti.png")
-        self.juttu_pieni = pygame.transform.rotozoom(self.juttu_iso, 0, 0.25)
+        self.juttu = pygame.transform.rotozoom(self.juttu_iso, 0, 0.25)
         self.pelimuuttujien_alustus()
 
     def pelimuuttujien_alustus(self):
         self.raketin_kulma = 0
-        self.raketin_pyorimisvauhti = 0
-        self.raketin_pyorimisvauhti_impulssi = 0
-        self.raketin_sijainti = (self.leveys / 2, self.korkeus / 2)
+        self.raketin_kulmanop = 0
+        self.raketin_kulmanop_impulssi = 0
+        self.raketin_xy = (self.leveys / 2, self.korkeus / 2)
         self.arvo_uusi_juttu()
         self.vauhti = 0
         self.hiiren_nappi_pohjassa = False
@@ -58,8 +58,8 @@ class Peli:
 
     def arvo_uusi_juttu(self):
         self.jutun_kulma = 360 * random()
-        self.jutun_pyorimisvauhti = 2 * (random() - 0.5)
-        self.jutun_sijainti = (randint(0, self.leveys), randint(0, self.korkeus))
+        self.jutun_kulmanop = 2 * (random() - 0.5)
+        self.jutun_xy = (randint(0, self.leveys), randint(0, self.korkeus))
 
     def tapahtuma(self, event):
         if event.type == pygame.QUIT:
@@ -72,9 +72,9 @@ class Peli:
         # Näppäimen painaminen alas ------------------------------
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                self.raketin_pyorimisvauhti_impulssi = 1
+                self.raketin_kulmanop_impulssi = 1
             elif event.key == pygame.K_RIGHT:
-                self.raketin_pyorimisvauhti_impulssi = -1
+                self.raketin_kulmanop_impulssi = -1
             elif event.key == pygame.K_SPACE:
                 self.voimanlisays = True
         # Näppäimen nosto ylös  ----------------------------------
@@ -86,7 +86,7 @@ class Peli:
             elif event.key == pygame.K_F11:
                 self.vaihda_kokoruututila()
             elif event.key in (pygame.K_LEFT, pygame.K_RIGHT):
-                self.raketin_pyorimisvauhti_impulssi = 0
+                self.raketin_kulmanop_impulssi = 0
             elif event.key == pygame.K_SPACE:
                 self.voimanlisays = False
                 self.laukaisu = True
@@ -106,15 +106,15 @@ class Peli:
 
         # Teleporttaus hiirellä
         if self.hiiren_nappi_pohjassa:
-            self.raketin_sijainti = pygame.mouse.get_pos()
+            self.raketin_xy = pygame.mouse.get_pos()
 
         # Raketin pyöriminen
-        self.raketin_pyorimisvauhti += self.raketin_pyorimisvauhti_impulssi
-        self.raketin_kulma = (self.raketin_kulma + self.raketin_pyorimisvauhti) % 360
-        self.raketin_pyorimisvauhti *= 0.88
+        self.raketin_kulmanop += self.raketin_kulmanop_impulssi
+        self.raketin_kulma = (self.raketin_kulma + self.raketin_kulmanop) % 360
+        self.raketin_kulmanop *= 0.88
 
         # Jutun pyöriminen
-        self.jutun_kulma = (self.jutun_kulma + self.jutun_pyorimisvauhti) % 360
+        self.jutun_kulma = (self.jutun_kulma + self.jutun_kulmanop) % 360
 
         # Rakettimoottorin voiman lisäys
         if self.voimanlisays:
@@ -129,15 +129,15 @@ class Peli:
         # Raketin liike
         vauhti_x = -self.vauhti * math.sin(self.raketin_kulma / 180 * math.pi)
         vauhti_y = -self.vauhti * math.cos(self.raketin_kulma / 180 * math.pi)
-        uusi_x = self.raketin_sijainti[0] + vauhti_x
-        uusi_y = self.raketin_sijainti[1] + vauhti_y
-        self.raketin_sijainti = (uusi_x, uusi_y)
+        uusi_x = self.raketin_xy[0] + vauhti_x
+        uusi_y = self.raketin_xy[1] + vauhti_y
+        self.raketin_xy = (uusi_x, uusi_y)
         self.vauhti *= 0.99
 
         # Raketin osuminen juttuun
         etaisyys_2 = (
-            (self.raketin_sijainti[0] - self.jutun_sijainti[0])**2 +
-            (self.raketin_sijainti[1] - self.jutun_sijainti[1])**2)
+            (self.raketin_xy[0] - self.jutun_xy[0])**2 +
+            (self.raketin_xy[1] - self.jutun_xy[1])**2)
         if etaisyys_2 < 10000:
             # Osui
             self.edellinen_vahennys = self.aikaa_jaljella
@@ -149,32 +149,39 @@ class Peli:
         # Tausta
         self.naytto.fill(TAUSTAVARI)
         # Juttu
-        kuva = pygame.transform.rotozoom(self.juttu_pieni, self.jutun_kulma, 1)
-        laatikko = kuva.get_rect(center=self.jutun_sijainti)
+        kuva = pygame.transform.rotozoom(self.juttu, self.jutun_kulma, 1)
+        laatikko = kuva.get_rect(center=self.jutun_xy)
         self.naytto.blit(kuva, laatikko.topleft)
         # Raketti
-        kuva = pygame.transform.rotozoom(self.raketti_pieni, self.raketin_kulma, 1)
-        laatikko = kuva.get_rect(center=self.raketin_sijainti)
+        kuva = pygame.transform.rotozoom(self.raketti, self.raketin_kulma, 1)
+        laatikko = kuva.get_rect(center=self.raketin_xy)
         self.naytto.blit(kuva, laatikko.topleft)
         # Voimapalkki
-        pygame.draw.rect(self.naytto, (0, 0, 0), (2, self.korkeus - 19, 102, 17))
-        pygame.draw.rect(self.naytto, (0, 255, 0), (3, self.korkeus - 18, self.voima, 15))
+        pygame.draw.rect(
+            self.naytto, (0, 0, 0), (2, self.korkeus - 19, 102, 17))
+        pygame.draw.rect(
+            self.naytto, (0, 255, 0), (3, self.korkeus - 18, self.voima, 15))
         # Suuntapallo
         suuntapallo_x = self.leveys - 35
         suuntapallo_y = self.korkeus - 35
         suuntavektori_x = -30 * math.sin(self.raketin_kulma / 180 * math.pi)
         suuntavektori_y = -30 * math.cos(self.raketin_kulma / 180 * math.pi)
-        pygame.draw.circle(self.naytto, (0, 0, 0), (suuntapallo_x, suuntapallo_y), 30)
-        pygame.draw.line(self.naytto, (255, 0, 0),
-                        (suuntapallo_x, suuntapallo_y),
-                        (suuntapallo_x + suuntavektori_x, suuntapallo_y + suuntavektori_y))
+        pygame.draw.circle(
+            self.naytto, (0, 0, 0), (suuntapallo_x, suuntapallo_y), 30)
+        pygame.draw.line(
+            self.naytto, (255, 0, 0),
+            (suuntapallo_x, suuntapallo_y),
+            (suuntapallo_x + suuntavektori_x, suuntapallo_y + suuntavektori_y))
         # Pisteet
         fontti = pygame.font.Font("font/SyneMono-Regular.ttf", 32)
-        teksti_kuva = fontti.render(f"Pisteet:{self.pisteet:3}", True, (128, 0, 128))
-        self.naytto.blit(teksti_kuva, (self.leveys - teksti_kuva.get_width() - 10, 10))
+        teksti_kuva = fontti.render(
+            f"Pisteet:{self.pisteet:3}", True, (128, 0, 128))
+        teksti_leveys = teksti_kuva.get_width()
+        self.naytto.blit(teksti_kuva, (self.leveys - teksti_leveys - 10, 10))
         # Aika
         fontti = pygame.font.Font("font/SyneMono-Regular.ttf", 32)
-        teksti_kuva = fontti.render(f"Aika:{self.aikaa_jaljella:6.1f}", True, (128, 0, 128))
+        teksti_kuva = fontti.render(
+            f"Aika:{self.aikaa_jaljella:6.1f}", True, (128, 0, 128))
         self.naytto.blit(teksti_kuva, (10, 10))
         # Loppu teksti
         if self.aikaa_jaljella <= 0:
@@ -201,5 +208,5 @@ class Peli:
         pygame.quit()
 
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     main()
